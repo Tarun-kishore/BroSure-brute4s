a51a65b428fdc6a1c5a6fbd8fa70cbdb3acd300a
Tarun Kishore <tarun2kishore@gmail.com>
2022-03-29 14:41:30 +0530

    added login and signup page:

--- src/public/script/signupForm.js
+++ src/public/script/signupForm.js
@@ -1,41 +1,50 @@
+const $emailAlert = document.querySelector("#emailAlert");
+const $passwordAlert = document.querySelector("#passAlert");
+const $form = document.querySelector("#signup");
+const $email = document.querySelector("#email");
+const $password = document.querySelector("#password");
+const $confirmPassword = document.querySelector("#confirmPassword");
 
-const $emailAlert = document.querySelector('#emailAlert')
-const $passwordAlert = document.querySelector('#passAlert')
-const $form = document.querySelector('#signup')
-const $email = document.querySelector('#email')
-const $password = document.querySelector('#password')
-const $confirmPassword = document.querySelector('#confirmPassword')
+$form.addEventListener("submit", (e) => {
+  $emailAlert.innerHTML = "";
+  $passwordAlert.innerHTML = "";
 
-$form.addEventListener('submit',(e)=>{
-    $emailAlert.innerHTML = ''
-    $passwordAlert.innerHTML = ''
+  $email.value = $email.value.toLowerCase();
 
-    $email.value = $email.value.toLowerCase()
+  e.preventDefault();
+  data = { email: $email.value };
+  fetch("/user/exist", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      let start = true;
+      if (response.exist) {
+        $emailAlert.innerHTML =
+          '<div class="form-error">Email ID already exists</div>';
+        start = false;
+      }
+      if ($password.value.length < 8) {
+        $passwordAlert.innerHTML =
+          '<div class="form-error">Password should be atleast 8 characters long</div>';
+        start = false;
+      }
 
-    data={email:$email.value}
-    fetch('/user/exist',{
-        method: 'post',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(data)
-    }).then((response)=> response.json()
-    ).then((response)=>{
-        if(response.exist){
-            $emailAlert.innerHTML = '<div class="form-error">Email ID already exists</div>'
-            e.preventDefault()
-        }
+      if ($password.value !== $confirmPassword.value) {
+        $passwordAlert.innerHTML =
+          '<div class="form-error">Password and confirm password should have same value</div>';
+        start = false;
+      }
+      if (start) {
+        $form.submit();
+      }
     })
-    
-    
-    if($password.value.length < 8){
-        $passwordAlert.innerHTML = '<div class="form-error">Password should be atleast 8 characters long</div>'
-        e.preventDefault()
-    }
-    
-    if($password.value !== $confirmPassword.value){
-        $passwordAlert.innerHTML = '<div class="form-error">Password and confirm password should have same value</div>'
-        e.preventDefault()
-    }
+    .catch((e) => {
+      console.log(e);
+    });
+});
 
-})
